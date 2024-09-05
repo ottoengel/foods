8ce849a289321d6e57399811ad7c074aa23c2d34
Otto <ottoengelhardt18@gmail.com>
2024-09-04 22:27:37 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import Header from "./_components/header";
+
 const Home = () => {
-  return ( 
-    <div className="mt-6 bg-red-500">
-       <h1>hello</h1>
-    </div>
-   );
-}
- 
+  return <Header />;
+};
+
 export default Home;
